b13d33c904b7724f07c8c84be0c1872148ccd988
Vladislav Vershinin <hookie@live.ru>
2020-05-30 19:26:15 +0300

    feat: impl navigation bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button'
 
 import {Home} from './components/Home'
 import {Department} from './components/Department'
 import {Users} from './components/Users'
+import {Navigation} from './components/Navigation'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
-    <Home/>
-    <Department/>
-    <Users/>
+
+      <h3 className="m-3 d-flex justify-content-center">Rostelecom test React JS app consuming ASP .NET Core Web Api</h3>
+      <h5 className="m-3 d-flex justify-content-center">Users and departments management portal</h5>
+
+      <Navigation/>
+
+<Switch>
+  <Route path = "/" component = {Home} exact/>
+  <Route path = "/department" component = {Department} />
+  <Route path = "/users" component = {Users}/>
+</Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
