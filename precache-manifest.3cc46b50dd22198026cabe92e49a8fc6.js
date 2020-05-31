e46ccabc3c8b35af982bbc0183b9084f9fd9a952
Vladislav Vershinin <hookie@live.ru>
2020-06-01 02:13:19 +0300

    Updates

---
+++ precache-manifest.3cc46b50dd22198026cabe92e49a8fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7adba97bc44223106830acf3fc68878",
+    "url": "/rostelecom-task-front/index.html"
+  },
+  {
+    "revision": "175d9cbc8b7197203cf9",
+    "url": "/rostelecom-task-front/static/css/main.c9f2f2bd.chunk.css"
+  },
+  {
+    "revision": "12d12001ffec6d248390",
+    "url": "/rostelecom-task-front/static/js/2.a362ee28.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/rostelecom-task-front/static/js/2.a362ee28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175d9cbc8b7197203cf9",
+    "url": "/rostelecom-task-front/static/js/main.10027eae.chunk.js"
+  },
+  {
+    "revision": "bc4020f334995d59c336",
+    "url": "/rostelecom-task-front/static/js/runtime-main.fb32f04e.js"
+  }
+]);
